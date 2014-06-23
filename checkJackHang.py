132bb49a7ccbc90132219c7b821fff4cfe1bbbea
Simon Albinsson <zinobg@zxz.se>
2014-06-23 12:24:13 +0200

    Docstrings and error handling.
    Added a few docstrings and some error handling in case the jack-thread
    wont die, this should not happen.

--- checkJackHang.py
+++ checkJackHang.py
@@ -6,15 +6,18 @@ import jack
 import sys
 import Queue
 
+
 class _testThread(multiprocessing.Process):
+	"""Processthread object to test jack-connectivity in."""
 	exitStatus=None
 	def __init__(self,queue):
+		"""queue -- any thrown exception will be placed on the queue must be a multiprocessing.Queue"""
 		self.queue=queue
 		super(_testThread, self).__init__()
 
 	def run(self):
 		try:
-			jack.attach("jackTest")
+			jack.attach("jackTest"+str(int(time.time())))
 			jack.detach()
 		except:
 			self.exitStatus=sys.exc_info()
@@ -26,24 +29,37 @@ class JackTimeOut(Exception):
 		return repr(self.value)
 
 def _safeQueueGet(queue):
+	"""helper to get the top of the queue"""
 	try:
 		return queue.get(False)
 	except Queue.Empty:
 		return None
 
-def testJack(timeOut):
+def testJack(timeOut=1):
+	"""Attempts to attach to Jack and returns any error that was encountered.
+	timeOut -- time out in seconds or fractional seconds to wait for attach/detach.
+
+	Returns None in case no error was encountered otherwise the exception object.
+
+	Might throw multiprocessing.ProcessError if it was unable to detach jack.
+	"""
 	q=multiprocessing.Queue()
 	t=_testThread(q)
 	t.start()
-	#print "thread started"
 	t.join(timeOut)
 	time.sleep(.1) #Because threads in python...
 	if not t.is_alive():
 		return _safeQueueGet(q)
 	else:
 		t.terminate()
-		return JackTimeOut("Jack attachment timed out after %f second%s"%(timeOut,("s" if timeOut!=1 else "")))
+		for i in range(20):
+			time.sleep(.1)
+			if not t.is_alive():
+				break
+		else:
+			multiprocessing.ProcessError("Unable to terminate jack thread")
+		return JackTimeOut("Jack attachment timed out after {} second{}".format(timeOut,("s" if timeOut!=1 else "")))
 
 
 if __name__=="__main__":
-	print testJack(2)
+	print ">>",testJack(2)
