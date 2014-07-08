f4d526dbc926e68208be796a6577e0b22110eff9
Simon Albinsson <zinobg@zxz.se>
2014-07-08 12:12:09 +0200

    Discarded the asyncronous socket skeleton.
    Better to start with the storage skeleton and add socket support later.
    Further more i will probably use the old threaded socket support since i
    am more familiar with threaded sockets than async.

--- ssmon/daemon/core.py
+++ ssmon/daemon/core.py
@@ -1,46 +1,32 @@
 #!/usr/bin/env python
-import asynchat
-
+import SocketServer
 if __name__ == "__main__":
 	import sys
 	import os
 	sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from utils.statuses import Pass,Fail
+from scheduler import scheduler
+import threading
 
 DEFPORT=41133		#Linux ephemeral base 32768 + 8365
-from asynchat_echo_handler import EchoHandler
 
-class Dispatcher(asyncore.dispatcher):
-    def __init__(self, address):
-        asyncore.dispatcher.__init__(self)
-        self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.bind(address)
-        self.address = self.socket.getsockname()
-        self.listen(1)
-        return
+class Storage():
+	def __init__(self,workers=[]):
+		self.scheduler=sheduler()
+		self.workers=workers
+		self.thread=threading.Thread(target=scheduler.run)
 
-    def handle_accept(self):
-        client_info = self.accept()
-        EchoHandler(sock=client_info[0])
-        self.handle_close() ##DEBUGGING ONLY
-        return
-    
-    def handle_close(self):
-        self.close()
+	def run(self):
+		if self.thread.is_alive():
+			raise Warning("Allredy running")
+		if self.thread()
+		try:
+			self.thread.start()
+		except RuntimeError:
+			self.thread=threading.Thread(target=scheduler.run)
+			self.thread.start()
 
-class http_request_handler(asynchat.async_chat):
 
-    def __init__(self, sessions, log):
-
-def _regtest_basicConnectivity():
-	""">>> s=socket.socket()
->>> s.connect( ('127.0.0.1', 41133))
->>> s.send('["noop"]\\n')
-9
->>> s.recv(100)
-'[];'
-"""
-	pass
 if __name__ == "__main__":
 	import socket
 	import doctest
