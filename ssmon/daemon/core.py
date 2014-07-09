8ff82d13cd9d65467389efe7899dc884e6a1076b
Simon Albinsson <zinobg@zxz.se>
2014-07-09 21:44:36 +0200

    Extended Storage class.
    Added the missing functions from the storage class diagram to the
    storage-class

--- ssmon/daemon/core.py
+++ ssmon/daemon/core.py
@@ -6,27 +6,52 @@ if __name__ == "__main__":
 	sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from utils.statuses import Pass,Fail
 from scheduler import scheduler
+import configreader
 import threading
 
 DEFPORT=41133		#Linux ephemeral base 32768 + 8365
 
 class Storage():
-	def __init__(self,workers=[]):
+	"""Class working as an intermediate storage between the scheduler 
+	ant the data-serving TCP-threads.
+	
+	It is meant as the primary "owner" of the worker-objects and the
+	scheduler. It will thusly setup a new scheduler at start and add any.
+	worker-objects to the scheduler.
+	"""
+	def __init__(self,workers=[],actors=[]):
 		self.scheduler=sheduler()
-		self.workers=workers
-		self.thread=threading.Thread(target=scheduler.run)
+		self.workers=set(workers)
+		self.actors=set(actors)
+		self.sthread=threading.Thread(target=scheduler.run)
+
+	def add_worker(self,worker):
+		self.workers.add(worker)
+
+	def remove_worker(self,worker):
+		self.workers.remove(worker)
+
+	def get_all_workers(self):
+		return self.workers
+
+	def set_interval(self,worker,interval):
+		self.scheduler.change_interval(worker,interval)
 
 	def run(self):
-		if self.thread.is_alive():
+		if selfs.sthread.is_alive():
 			raise Warning("Allredy running")
-		if self.thread()
+		if self.sthread()
 		try:
-			self.thread.start()
+			self.sthread.start()
 		except RuntimeError:
-			self.thread=threading.Thread(target=scheduler.run)
-			self.thread.start()
+			self.sthread=threading.Thread(target=scheduler.run)
+			self.sthread.start()
 
 
+	class core():#should perhaps use python-daemon from PIP (PEP3143)
+		def __init__(self,configfile):
+			self.conf=conf=configreader.configreader(configfile)
+			self.storage=storage=storage(self.conf.generateworkers())
 if __name__ == "__main__":
 	import socket
 	import doctest
