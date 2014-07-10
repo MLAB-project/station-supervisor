419d8ae040a5c0fc7c2985daa4e6828a4e3fa667
Simon Albinsson <zinobg@zxz.se>
2014-07-10 17:47:47 +0200

    server-config-parser first draft finished
    The confing reader can now parse hostname and port from a [server] section

--- ssmon/daemon/configreader.py
+++ ssmon/daemon/configreader.py
@@ -20,31 +20,113 @@ def splitfile(s):
 	>>> splitfile(StringIO.StringIO('[Section]\\ndata=foo'))
 	[['[', 'Section', ']'], ['data', '=', 'foo']]
 	"""
-	lexer=shlex.shlex(s)
+	lexer=shlex.shlex(s,getattr(s,"name",None))
 	lexer.commenters="#"
 	lexer.wordchars+=".-+()/&%!?"
 	lexer.whitespace=", \t"
-	f=[]
+	f=[] #could be more efficient by yeilding row instead of building this
 	row=[]
-	for i in lexer:
-		if i.isdigit():
-			row.append(int(i))
-		elif i.replace(".","").isdigit():
-			row.append(float(i))
-		elif i[0] in '\'"' and i[-1]==i[0]:
-			row.append(i[1:-1])
-		elif "\n" in i or "\r" in i:
+	try:
+		for i in lexer:
+			if i.isdigit():
+				row.append(int(i))
+			elif i.replace(".","",1).isdigit():
+				row.append(float(i))
+			elif i[0] in '\'"' and i[-1]==i[0]:
+				row.append(i[1:-1])
+			elif "\n" in i or "\r" in i:
+				f.append(row)
+				row=[]
+			elif i:
+				row.append(i)
+		if len(row)>0:
 			f.append(row)
-			row=[]
-		elif i:
-			row.append(i)
-	f.append(row)
-	return f
-def parse(filename):
-	config = ConfigParser.ConfigParser()
-	config.read("c:\\tomorrow.ini")
+		return f
+	except ValueError,ex:
+		raise ValueError(lexer.error_leader()+ ex.message)
 
+def _regtest_splitparse_fail_quote():
+	"""
+	>>> import StringIO
+	>>> s=StringIO.StringIO('[Section]\\ndata="foo')
+	>>> s.name="myFile"
+	>>> splitfile(s)
+	Traceback (most recent call last):
+	ValueError: "myFile", line 2: No closing quotation
+	"""
+
+
+def parse(filename):
+	try:
+		if hasattr(filename,"read"):
+			f=filename
+		else:
+			f= open(filename,"r")
+		config=splitfile(f)
+	finally:
+		f.close()
+	
+	structure={"server":{}, "workers":[], "triggers":[], "actors":[]}
+	section=None
+	stack=[]
+	errors=[]
+	for i in config:
+		if i[0]=="[" and i[-1]=="]":
+			if len(i) >= 3:
+				oldsection=section
+				section=i[1].lower()
+				if oldsection:
+					parse_section(structure,oldsection,stack,errors)
+					stack=[]
+		else:
+			stack.append(i)
+	parse_section(structure,section,stack,errors)
+	if errors:
+		print errors
+	return structure
+
+def parse_section(structure,section,stack,errors):
+	classes={"worker":parse_worker,"server":parse_server,
+			"tigger":parse_trigger,"actor":parse_actor}
+	try:
+		classes[section](structure,section,stack)
+	except Exception, e:
+		errors.append(e)
+
+
+def parse_worker(structure,section,stack):
+	pass
+
+def parse_server(structure,section,stack): 
+	opts={}
+	args=dict([i for i in post if i!="="] for post in stack)
+	opts["port"]=args.pop("port",41133)
+	opts["hostname"]=args.pop("hostname","localhost")
+	structure["server"]=opts
+	if len(args)!=0:
+		raise RuntimeWarning("un used arguments to server"+repr(args.keys()))
+
+def parse_trigger(structure,section,stack):
+	pass
+def parse_actor(structure,section,stack):
+	pass
+
+def _regtest_serverparse():
+	"""
+	>>> import StringIO
+	>>> s=StringIO.StringIO('[server]\\nport=1010\\nhostname="foo"\\n[worker]')
+	>>> s.name="myFile"
+	>>> p=parse(s)
+	>>> p["server"]["port"]
+	1010
+	>>> p["server"]["hostname"]
+	'foo'
+	>>> s=StringIO.StringIO('[server]\\nport=1010\\nhostname="foo"\\nmung="mung"\\n[worker]')
+	>>> s.name="myFile"
+	>>> p=parse(s)
+	[RuntimeWarning("un used arguments to server['mung']",)]
+	"""
 
 if __name__ == "__main__":
-	doctest.testmod()
+	doctest.testmod(optionflags=doctest.ELLIPSIS)
 
