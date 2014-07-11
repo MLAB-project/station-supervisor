8a2a35318fb0bfe9a76916b8db5f1070de609d10
Simon Albinsson <zinobg@zxz.se>
2014-07-11 10:23:46 +0200

    Added comments

--- ssmon/daemon/worker.py
+++ ssmon/daemon/worker.py
@@ -2,7 +2,22 @@
 
 
 class worker():
+	"""Class to contain periodic test case.
+
+	Workers are fed in to a scheduler and runs periodic tests.
+	The results of these tests are fed to a list of triggers.
+	These Triggers should then invoke an action via the trrigger class.
+	"""
 	def __init__(self,interval,test,args,limits,triggers=[]):
+		"""
+		interval -- the time between runs in seconds.
+		test     -- name of the test-module to run.
+		args     -- the argument to be sent as the first argument to the
+							tests  check function.
+		limits   -- the second argument to the tests check()-function.
+		triggers -- a trigger function receives all the Pass/Fail results
+							from the check and takes appropriate action.
+		        """
 		if interval <1:
 			raise ValueError("interval must be 1 second or greater")
 		self._interval=interval
@@ -13,19 +28,23 @@ class worker():
 		self._result=[]
 		self.status="Initial"
 
-	def get_interval(self):
-		return self._interval
-
-	def set_interval(self,interval):
-		self._interval=interval
-
 	def run(self):
+		"""Runs the actual test and appropriate triggers.
+
+		This method is called by the scheduler at the requested interval.
+		"""
 		self.status="Running"
 		self._result=self._test.check(self._arg,self._limits)
 		self.status="Proccessing triggers"
 		for trigger in self._triggers:
 			trigger.validate(self._result)
 		self.status="Idle"
+
+	def get_interval(self):
+		return self._interval
+
+	def set_interval(self,interval):
+		self._interval=interval
 
 	def get_last_result(self):
 		return self._result
