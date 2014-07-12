00682dbac6627f727e14176fd4476d00266798cd
Simon Albinsson <zinobg@zxz.se>
2014-07-12 15:37:08 +0200

    Started reimplementing senors in to a class.
    Realized that the data-storage was not flexible enough when using
    function-based sensors. Created a base-class for usage in a more OO
    approach.

---
+++ ssmon/sensors/basesensor.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+if __name__ == "__main__":
+	import sys
+	import os
+	sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+from utils.statuses import Pass,Fail
+
+class basesensor(object):
+	def __init__(self,target,limits):
+		"""Base class for a sensor,
+
+		target -- A refference to what this sensor should be monitoring
+		for example a device like "/dev/sda1" or similar. Even if the
+		sensor does not need this it should accept an argument, might be
+		None.
+		
+		limits -- a list of bounds which the sensors should check against
+		it is upp to the sensor-implementation to decide format and if it
+		should be considdered upper or lower bounds.
+		"""
+		self._value=None
+		self._checks=None
+		raise NotImplemented("run not implemented")
+
+	def check(self):
+		"""Runs the tests and compares to the threshold values.
+
+		returns a list of Pass and Fail objects. These should allso be
+		stored inside the self._checks atribute."""
+		raise NotImplemented("check not implemented")
+
+	@property
+	def lastvalue(self):
+		return self._value
+
+	@property
+	def lastcheck(self):
+		return self._checks
+
+	@property
+	def repr_result(self):
+		"""Should return a human readable representation of the last last
+		run.
+		
+		for example: "19/200GiB (22.5%) used"
+		"""
+		raise NotImplemented("result_repr not implemented")
+
+	@property
+	def repr_check(self):
+		"""Should return a human readable representation of the last
+		check. Prefferably with any fails first.
+		
+		for example: "<20% used (FAIL), >90GiB Free (Pass)"
+		"""
+		raise NotImplemented("repr_check not implemented")
+
+	def __str__(self):
+		"""A human readable representation of the sensor.
+		
+		For example "Disk usage: /dev/sda1" """
+		raise NotImplemented("str not implemented")
+
+	if __name__ == "__main__":
+		import doctest
+		doctest.testmod()
+
