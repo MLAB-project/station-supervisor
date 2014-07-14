b15a589249fea302fd628da0803f238c9ee0b8ef
Simon Albinsson <zinobg@zxz.se>
2014-07-14 22:25:15 +0200

    Started converting ntp-drift to class based.

--- ssmon/sensors/ntpdrift.py
+++ ssmon/sensors/ntpdrift.py
@@ -4,7 +4,10 @@ import os
 import sys
 if __name__ == "__main__":
 	sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
 from utils.statuses import Pass,Fail
+from sensorbase import sensorbase
+
 
 path=os.getenv("PATH").split(":")
 path+=[p.replace("bin","sbin") for p in path if "bin" in p ]
@@ -20,31 +23,48 @@ for p in path:
 assert ntppath!=None, "Could not locate the ntpdate program"
 	
 
-def run(ntpHost):
-	"""Querys a given ntp host or ntp pool and returns the time difference """
-	result=sub.check_output([ntppath, "-q",ntpHost]).strip().splitlines()
-	summaryline=result[-1].split()
-	diff=summaryline[summaryline.index("sec")-1]
-	return float(diff)
-
-def repr(host):
-	return "time drift relative to {}: {:.2f}".format(host,run(host))
-
-def check(ntpHosts):
-	"""Takes a list of host-limit tuples and returns wether the system-time differs less than the limit value from the given ntp-host
-	To check that the time is off with less than one second from the europe ntp-pool (should,probably be OK)
-	and with less than 0.1 ms from ntp1.sp.se (should, probably fail)
->>> check([("europe.pool.ntp.org",1.0),("ntp1.sp.se",0.0001)])
-[Pass, Fail]
+class ntpdrift(sensorbase):
+	"""Sensor to check if systems clock is in sync with an NTP source
+	
+	Depends on the ntpdate program.
 	"""
-	res=[]
-	for host,limit in ntpHosts:
-		diff=run(host)
-		if abs(diff)<=limit:
-			res.append(Pass())
-		else:
-			res.append(Fail("{} was {:.2f} seconds off".format(host,diff)))
-	return res
+
+	def __init__(self,ntpServer,limits):
+		self.server=ntpServer
+		self.limits=limits
+		self._value=None
+		self._checks=None
+		
+	def _run(self,ntpHost):
+		"""Querys a given ntp host or ntp pool and returns the time difference"""
+		result=sub.check_output([ntppath, "-q",ntpHost]).strip().splitlines()
+		summaryline=result[-1].split()
+		diff=summaryline[summaryline.index("sec")-1]
+		self._value=float(diff)
+		return self._value
+
+	def check(self):
+		"""checks the ntp-host against the limit values.
+
+		Returns a list of Pass/Fail for each limit.
+
+		For example to check if the time is within 2 seconds and if it
+		is within 0.1ms from these servers.
+	>>> ntpdrift("europe.pool.ntp.org",[2,0.00001]).check()
+	[Pass, Fail]
+	>>> ntpdrift("ntp1.sp.se",[2,0.00001]).check()
+	[Pass, Fail]
+
+		"""
+		res=[]
+		diff=self._run(self.server)
+		
+		for limit in self.limits:
+			if abs(diff)<=limit:
+				res.append(Pass())
+			else:
+				res.append(Fail("{} was {:.2f} seconds off".format(self.server,diff)))
+		return res
 
 if __name__ == "__main__":
 	import doctest
