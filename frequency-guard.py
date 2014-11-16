69fbca8c8a403ab97c2bc7ced920c7fa4b7e20aa
kakl <kakl@astro.cz>
2014-11-16 22:49:47 +0100

    Logfile output rewriten to CSV format.

--- frequency-guard.py
+++ frequency-guard.py
@@ -1,61 +1,74 @@
 #!/usr/bin/python
 # 
-# Software for frequency management on RMDS station
-#
-# 
+# Sample of measuring and frequency correction with ACOUNTER02A
 
 import time
 import datetime
 import sys
 from pymlab import config
+import os
 
-#### System configuration ###########################################
+port    = 1
+req_freq = 20.0 #7.1588
 
-port = 1  # I2C port number
-frequency =
+#### Sensor Configuration ###########################################
 
-cfg = config.Config(
-    i2c = {
-        "port": port,
-    },
-    bus = [
-        {
-            "type": "i2chub",
-            "address": 0x70,
-	       	"children": [
-                        { "name":"counter", "type":"acount02", "channel": 2, },
-                        { "name":"clkgen", "type":"clkgen01", "channel": 5, },
-		    ],
-        },
-    ],
-)
-
-#### System Initialization ###############################################
+cfg = config.Config()
+cfg.load_file("./config_file.py")
 
 cfg.initialize()
-print "RMDS Station frequency management test software \r\n"
+
+print "RMDS Station Local oscillator tunning utility \r\n"
 fcount = cfg.get_device("counter")
 fgen = cfg.get_device("clkgen")
 time.sleep(0.5)
+fcount.route()
 frequency = fcount.get_freq()
+fgen.route()
+rfreq = fgen.get_rfreq()
+hsdiv = fgen.get_hs_div()
+n1 = fgen.get_n1_div()
+
+fcount.route()
+fcount.set_GPS()	# set GPS configuration
+
 
 #### Data Logging ###################################################
 
 try:
-    with open("frequency.log", "a") as f:
         while True:
             now = datetime.datetime.now()
+            filename = time.strftime("%Y%m%d%H")+"0000_freq.csv"
+            if not os.path.exists(filename):
+                with open(filename, "a") as f:
+                    f.write('#timestamp,LO_Frequency \n')
+
             if (now.second == 15) or (now.second == 35) or (now.second == 55):
+                fcount.route()
                 frequency = fcount.get_freq()
+                with open(filename, "a") as f:
+                    f.write("%.1f,%.1f\n" % (time.time(), frequency))
+
+
                 if (len(sys.argv) == 3):
-                    regs = fgen.set_freq(frequency/1e6, float(eval(sys.argv[2])))              
+                    fgen.route()
+                    regs = fgen.set_freq(frequency/1e6, float(req_freq))
                 now = datetime.datetime.now()
 
-            sys.stdout.write("frequency: " + str(frequency) + " Hz  Time: " + str(now.second))
-            f.write("%d\t%s\t%.3f\n" % (time.time(), datetime.datetime.now().isoformat(), frequency))
+            fgen.route()
+            rfreq = fgen.get_rfreq()
+            hsdiv = fgen.get_hs_div()
+            n1 = fgen.get_n1_div()
+            fdco = (frequency/1e6) * hsdiv * n1
+            fxtal = fdco / rfreq
+
+            sys.stdout.write("Current Freq.: " + str(frequency/1e6) + " MHz ")
+            sys.stdout.write("Req. Freq.: " + str(req_freq) + "MHz  Freq diff.: " + str(frequency - req_freq*1e6) + "Hz Time: " + str(now.second) + "s \r")
 
             sys.stdout.flush()
+
             time.sleep(0.9)
+
 except KeyboardInterrupt:
     sys.stdout.write("\r\n")
     sys.exit(0)
