96b3d16f18f686ebf337aeda3b5364f74b5494f0
Jakub Kakona <kaklik@mlab.cz>
2014-12-03 19:24:40 +0100

    start and stop bash scripts were added.

---
+++ start.sh
@@ -0,0 +1,18 @@
+ulimit -c unlimited
+~/Bolidozor/frequency-guard.py&
+jackd -c system -p128 -m -dalsa -dhw:CODEC -r48000 -p4096 -n4 -m -C -i2&
+sleep 3
+#qjackctl&
+#~/Bolidozor/pysdr/pysdr-waterfall -d ~/Bolidozor/pysdr/detectors/meteor_echo.py -b 8192&
+#sleep 5
+#jack_connect system:capture_1 pysdr:input_i
+#jack_connect system:capture_2 pysdr:input_q
+#~/Bolidozor/whistle/whistle -p freqx,-10400:kbfir,41,0,400,100:freqx,400:amplify,20&
+#sleep 3
+#jack_connect system:capture_1 whistle:input_i
+#jack_connect system:capture_2 whistle:input_q
+#jack_connect whistle:output_i system:playback_1
+#jack_connect whistle:output_q system:playback_2
+~/Bolidozor/uFlu/radio-observer -c ~/Bolidozor/uFlu/uFlu-R0.json&
+sleep 3
+#jack_connect TEST-R0:midi_out pysdr:input_events
---
+++ stop.sh
@@ -0,0 +1,7 @@
+killall frequency-guard.py
+killall jackd
+killall qjackctl
+killall waterfall.py
+killall whistle
+killall radio-observer
+
