ae5154b53b9233cdf0dbc591e315a37be6d2cff4
Jakub Kakona <kaklik@mlab.cz>
2015-03-21 23:52:55 +0100

    removed duplicite kills.

--- stop.sh
+++ stop.sh
@@ -1,16 +1,7 @@
 killall frequency-guard.py
+killall jackd
 killall qjackctl
 killall waterfall.py
 killall whistle
 killall radio-observer
-killall jackd
-killall run.py
-
-killall frequency-guard.py
-killall qjackctl
-killall waterfall.py
-killall whistle
-killall radio-observer
-killall jackd
-killall run.py
 
