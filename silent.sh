b0a400b48e9bd18c5f920e99d330a4b5586b5718
kakl <kakl@astro.cz>
2015-05-30 16:01:53 +0200

    visualisation scripts was modified

--- silent.sh
+++ silent.sh
@@ -1,8 +1,11 @@
 killall qjackctl
 killall pysdr-waterfall
+killall python
 killall whistle
 killall xgps
+killall alsa_out
 killall qjackctl
 killall pysdr-waterfall
 killall whistle
 killall xgps
+killall alsa_out
--- visualisation.sh
+++ visualisation.sh
@@ -5,13 +5,13 @@ qjackctl&
 sleep 5
 jack_connect system:capture_1 pysdr:input_i
 jack_connect system:capture_2 pysdr:input_q
-~/git/pysdr/whistle/whistle -p freqx,-26300:kbfir,201,0,400,200:freqx,500:amplify,200&
+~/git/pysdr/whistle/whistle -p freqx,-26300:kbfir,201,0,400,200:freqx,500:amplify,10&
 sleep 3
 jack_connect system:capture_1 whistle:input_i
 jack_connect system:capture_2 whistle:input_q
 jack_connect radio-observer:midi_out pysdr:input_events
-#alsa_out -d hw:2 -r96000&
-#sleep 2
-#jack_connect whistle:output_i alsa_out:playback_1
-#jack_connect whistle:output_q alsa_out:playback_2
+alsa_out -d hw:0&
+sleep 2
+jack_connect whistle:output_i alsa_out:playback_1
+jack_connect whistle:output_q alsa_out:playback_2
 xgps&
