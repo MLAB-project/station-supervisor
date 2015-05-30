2f8e058c69fecd57728c6e64361dcf0f11ba61b6
kakl <kakl@astro.cz>
2015-05-30 18:57:59 +0200

    bomben fest und idioten sicher tune

--- visualisation.sh
+++ visualisation.sh
@@ -5,7 +5,7 @@ qjackctl&
 sleep 5
 jack_connect system:capture_1 pysdr:input_i
 jack_connect system:capture_2 pysdr:input_q
-~/git/pysdr/whistle/whistle -p freqx,-26300:kbfir,201,0,400,200:freqx,500:amplify,10&
+~/git/pysdr/whistle/whistle -p freqx,-26300:kbfir,201,70,400,200:freqx,500:amplify,10&
 sleep 3
 jack_connect system:capture_1 whistle:input_i
 jack_connect system:capture_2 whistle:input_q
