770c2fe6ccfe49d65b9fcde3c2ae3e06886d96e9
Martin Povišer <martin.povik@gmail.com>
2015-08-10 17:15:45 +0200

    Add scripts from latest, JACK-less station image.

---
+++ mount-sd-card.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+for dev in $(ls /dev | grep '^mmcblk[0-9]$')
+do
+	if [ $(mount | grep -c /dev/$dev ) == 0 ]
+	then
+		mount /dev/${dev}p1 /home/odroid/bolidozor
+		exit 0
+	fi
+done
