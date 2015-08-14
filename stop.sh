d286d33232f2c8009fd6cfea68a7fe0ec699b614
roman-dvorak <roman-dvorak@email.cz>
2015-08-15 07:30:39 +0930

    commit repair

--- stop.sh
+++ stop.sh
@@ -2,15 +2,6 @@ killall frequency-guard.py
 killall servestream
 killall servecmd
 killall sdr-widget
-<<<<<<< HEAD
-=======
-killall radio-observer
-
-killall frequency-guard.py
-killall servestream
-killall servecmd
-killall sdr-widget
->>>>>>> 770c2fe6ccfe49d65b9fcde3c2ae3e06886d96e9
 killall radio-observer
 
 killall frequency-guard.py
