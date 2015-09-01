596e536f5ea36d889850addb56c7c90f980fa531
Jakub Kakona <kaklik@mlab.cz>
2015-09-01 13:48:54 +0200

    new station startup system with NTP-waiting feature.

---
+++ mount-sd-card.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+for dev in $(ls /dev | grep '^mmcblk[0-9]$')
+do
+	# TODO
+	if [ $(mount | grep -c /dev/$dev ) == 0 ]
+	then
+		mount /dev/${dev}p1 /home/odroid/bolidozor
+		exit 0
+	fi
+done
--- start.sh
+++ start.sh
@@ -1,10 +1,37 @@
 #!/bin/sh
-#ulimit -c unlimited
-cd ~/Bolidozor/station-supervisor/
-./frequency-guard.py&
-jackd -r -t5000 -u -c system  -m -dalsa -dhw:1 -r96000 -p2048 -n2 -C -i2 -s&
-sleep 3
-~/git/radio-observer/radio-observer -c ~/Bolidozor/TEST-R0/Bolidozor.json&
-~/svn/jacktrip/src/jacktrip -s&
-cd ~/git/RMDS-data-uploader
-./run.py
+
+ntp-wait -v
+sleep 5
+
+JSON_CONFIG="/home/odroid/bolidozor/station/Bolidozor.json"
+BUS_CONFIG="/home/odroid/bolidozor/station/bus_config.py"
+
+ulimit -c unlimited
+
+cd ~/repos/station-supervisor
+
+if ! pidof -x frequency-guard.py > /dev/null; then
+	./frequency-guard.py $JSON_CONFIG $BUS_CONFIG > /dev/null &
+fi
+
+cd ~/repos/signal-piping-tools
+
+rm -rf 3731_taps
+./fir_taps -n 512 -r 96000 -c 1000 -w hanning > 3731_taps
+
+if ! pidof sdr-widget > /dev/null; then
+	{ ./sdr-widget -r 96000 | buffer & } | ./servestream -d -p 3701
+fi
+
+./servecmd -d -p 3731 'nc localhost 3701 | buffer -s 64k -m 2m | ./x_fir_dec -b 128 96000 26500 48 3731_taps'
+
+if ! pidof radio-observer > /dev/null; then
+	~/repos/radio-observer/radio-observer -c ~/bolidozor/station/Bolidozor.json &
+fi
+
+cd ~/repos/RMDS-data-uploader
+
+if ! pidof -x run.py > /dev/null; then
+	./run.py &
+fi
+
