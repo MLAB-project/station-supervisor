5d3d01035bf0a3eace8e8b7df129707d3de500fd
Jakub <kaklik@mlab.cz>
2016-08-04 21:34:11 +0000

    start-up script symplified for Ionozor needs.

--- start.sh
+++ start.sh
@@ -3,16 +3,9 @@
 sleep 10
 ntp-wait -v
 
-JSON_CONFIG="/home/odroid/bolidozor/station/Bolidozor.json"
-BUS_CONFIG="/home/odroid/bolidozor/station/bus_config.py"
+JSON_CONFIG="/home/odroid/ionozor/station/Ionozor.json"
 
 ulimit -c unlimited
-
-cd ~/repos/station-supervisor
-
-if ! pidof -x frequency-guard.py > /dev/null; then
-	./frequency-guard.py $JSON_CONFIG $BUS_CONFIG > /dev/null &
-fi
 
 cd ~/repos/signal-piping-tools
 
@@ -26,13 +19,13 @@ fi
 ./servecmd -d -p 3731 'nc localhost 3701 | buffer -s 64k -m 2m | ./x_fir_dec -b 128 96000 26500 48 3731_taps'
 
 if ! pidof radio-observer > /dev/null; then
-	~/repos/radio-observer/radio-observer -c ~/bolidozor/station/Bolidozor.json &
+	~/repos/radio-observer/radio-observer -c ~/ionozor/station/Ionozor.json &
 fi
 
 cd ~/repos/RMDS-data-uploader
 
 
 if ! pidof -x data_upload.py > /dev/null; then
-	./data_upload.py &
+	./data_upload.py ~/ionozor/station/Ionozor.json &
 fi
 
