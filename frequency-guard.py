9f664960768ef3ca47f77b10f67ae2535df726d8
Jakub Kakona <kaklik@mlab.cz>
2016-09-23 12:43:29 +0200

    Sensitivity to GPS reset decreased.

--- frequency-guard.py
+++ frequency-guard.py
@@ -70,7 +70,7 @@ while True:
 		    filename = path + time.strftime("%Y%m%d%H", time.gmtime())+"0000_"+StationName+"_freq.csv"
 		    if not os.path.exists(filename):
 		        with open(filename, "a") as f:
-		            f.write('#timestamp,LO_Frequency \n')
+		            f.write('#timestamp,LO_Frequency,Hit \n')
 
 		    if (now.second == 15) or (now.second == 35) or (now.second == 55):
 		        fcount.route()
@@ -82,11 +82,11 @@ while True:
 			    regs = fgen.set_freq(frequencies.mean()/1e6, float(req_freq/1e6))
 			    frequencies = np.append(frequencies, current_freq)
 			    freq_corr="Y"
-			elif (abs(freq_error) > (30*frequencies.std())) or (frequencies.std() > 1000):
+			elif (abs(freq_error) > (1e6*frequencies.std())) or (frequencies.std() > 1e6):
 			    """ If frequency error is unrealistically big, we reset the GPS firstly before setting the new oscillator frequency"""
 			    fcount.route()
 			    fcount.set_GPS()	# set GPS configuration
-			    time.sleep(10)
+			    time.sleep(20)
 
 			    while not ((now.second == 15) or (now.second == 35) or (now.second == 55)):
 				pass
