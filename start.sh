1036a544cc56563b1ceef71accfd3f63d175c37f
Jakub Kakona <kaklik@mlab.cz>
2019-07-01 11:53:55 +0200

    Addig support for lightning detection station

--- start.sh
+++ start.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-killall RTbolidozor.py
+./afe5801_conf.py
+echo 960 > /sys/class/gpio/export
+mount /dev/mmcblk0p3 /data
+
 sleep 10
 ntp-wait -v
 
@@ -9,33 +12,14 @@ BUS_CONFIG="/home/odroid/bolidozor/station/bus_config.py"
 
 ulimit -c unlimited
 
-cd ~/repos/station-supervisor
-
-if ! pidof -x frequency-guard.py > /dev/null; then
-	./frequency-guard.py $JSON_CONFIG $BUS_CONFIG > /dev/null &
+if ! pidof -x lightning_example.py > /dev/null; then
+	python ~/repos/examples/lightning_example.py 0 > ~/lightning_log &
 fi
 
-cd ~/repos/signal-piping-tools
-
-rm -rf 3731_taps
-./fir_taps -n 512 -r 96000 -c 1000 -w hanning > 3731_taps
-
-if ! pidof sdr-widget > /dev/null; then
-	{ ./sdr-widget -r 96000 | buffer & } | ./servestream -d -p 3701
-fi
-
-if ! pidof ./servecmd > /dev/null; then
-    ./servecmd -d -p 3731 'nc localhost 3701 | buffer -s 64k -m 2m | ./x_fir_dec -b 128 96000 26500 48 3731_taps'
-fi
-
-if ! pidof radio-observer > /dev/null; then
-	~/repos/radio-observer/radio-observer -c $JSON_CONFIG | python ~/repos/station-supervisor/RTbolidozor.py $JSON_CONFIG&
-fi
-
-cd ~/repos/data-uploader
+cd ~/drivers
 
 
-if ! pidof -x dataUpload.py > /dev/null; then
-	./dataUpload.py $JSON_CONFIG > /dev/null &
+if ! pidof -x trigger > /dev/null; then
+	./trigger -pre 213 -post 10 -recdir /data > /dev/null &
 fi
 
