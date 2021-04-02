c315bae1a0fc007fb1f1dfa22bf18bcf9abba7ae
Jakub Kakona <kaklik@mlab.cz>
2021-04-02 02:58:19 +0000

    removing oscillator frequency setup.

--- start.sh
+++ start.sh
@@ -4,19 +4,10 @@
 echo 960 > /sys/class/gpio/export
 mount /dev/mmcblk0p3 /data
 
-JSON_CONFIG="Lightning.json"
-BUS_CONFIG="bus_config.py"
-
-./frequency-set.py $JSON_CONFIG $BUS_CONFIG
-
 sleep 10
 ntp-wait -v
 
 ulimit -c unlimited
-
-#if ! pidof -x lightning_example.py > /dev/null; then
-#	python ~/repos/pymlab/examples/lightning_example.py 0 > ~/lightning_log &
-#fi
 
 cd ~/drivers
 
