26c1e7000762d3f42a40fbf736402380013daa7d
Martin Povišer <povik@protonmail.com>
2022-09-05 18:48:38 +0000

    Update start.sh/stop.sh for triggernew

--- start.sh
+++ start.sh
@@ -3,15 +3,24 @@
 ./afe5801_conf.py
 echo 960 > /sys/class/gpio/export
 mount /dev/mmcblk0p3 /data
-
+mount -t tmpfs tmp /tmp
 sleep 10
 ntp-wait -v
 
 ulimit -c unlimited
 
+if ! pgrep ourperiph.py > /dev/null; then
+	~/repos/regweb/ourperiph.py &
+fi
+
+mkdir -p /tmp/snaps
+if ! pgrep cleanup.sh > /dev/null; then
+	./cleanup.sh &
+fi
+
 cd ~/drivers
 
-
-if ! pidof -x trigger > /dev/null; then
-	./trigger -pre 210 -post 13 -nofir -recdir /data > /dev/null &
+if ! pidof -x triggernew > /dev/null; then
+	./triggernew -pre 210 -post 13 -nofir -recdir /data \
+		-spre 1 -spost 2 -snapdir /tmp/snaps/ > /dev/null &
 fi
